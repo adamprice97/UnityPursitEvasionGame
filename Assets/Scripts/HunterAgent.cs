2e19db9b0825fc99a6626d8a01cbbc140b89e7fe
adamprice97 <62331269+adamprice97@users.noreply.github.com>
2020-07-14 11:17:24 +0100

    Add files via upload

---
+++ Assets/Scripts/HunterAgent.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+public class HunterAgent : Agent
+{
+    public GameObject target;
+    public GameObject bodyObject;
+    public GameObject ally1;
+    public GameObject ally2;
+    Rigidbody m_Rb;
+    Vector3 m_LookDir;
+    public float strength = 10f;
+    public float range = 15f;
+    EnvironmentParameters m_ResetParams;
+
+    public override void Initialize() {
+        m_Rb = gameObject.GetComponent<Rigidbody>();
+        m_LookDir = Vector3.zero;
+    }
+
+    public override void CollectObservations(VectorSensor sensor) {      
+        sensor.AddObservation(bodyObject.transform.localPosition.x);
+        sensor.AddObservation(bodyObject.transform.localPosition.z);
+        sensor.AddObservation(ally1.transform.localPosition.x - bodyObject.transform.localPosition.x);
+        sensor.AddObservation(ally1.transform.localPosition.z - bodyObject.transform.localPosition.z);
+        sensor.AddObservation(ally2.transform.localPosition.x - bodyObject.transform.localPosition.x);
+        sensor.AddObservation(ally2.transform.localPosition.z - bodyObject.transform.localPosition.z);
+        sensor.AddObservation(target.transform.localPosition.x - bodyObject.transform.localPosition.x);
+        sensor.AddObservation(target.transform.localPosition.z - bodyObject.transform.localPosition.z);
+        sensor.AddObservation(target.transform.rotation);
+    }
+
+    public override void OnActionReceived(float[] vectorAction) {
+        for (var i = 0; i < vectorAction.Length; i++) {
+            vectorAction[i] = Mathf.Clamp(vectorAction[i], -1f, 1f);
+        }
+        var x = vectorAction[0];
+        var z = vectorAction[1];
+        m_Rb.AddForce(new Vector3(x, 0, z) * strength);
+
+        AddReward(-0.001f);
+
+        m_LookDir = new Vector3(x, 0, z);
+
+    }
+
+    public override void OnEpisodeBegin() {
+
+        float angle =  Random.Range(0, Mathf.PI*2);
+        gameObject.transform.localPosition = new Vector3(Mathf.Sin(angle)*range,0.5f,Mathf.Cos(angle)*range);
+
+        m_Rb.velocity = default(Vector3);
+    }
+    void FixedUpdate() {
+
+        if (Mathf.Abs(DistanceToCentre(transform.position)) > range + 1) { 
+            AddReward(-3f);
+            HunterAgent hunt1 = ally1.GetComponent(typeof(HunterAgent)) as HunterAgent;
+            hunt1.EndEpisode();
+            HunterAgent hunt2 = ally2.GetComponent(typeof(HunterAgent)) as HunterAgent;
+            hunt2.EndEpisode();
+            EndEpisode();
+            return;
+        }
+
+        if (this.StepCount > 1000) {
+            EndEpisode();
+            Pray p = target.GetComponent(typeof(Pray)) as Pray;
+            p.Respawn();
+        }
+    }
+
+    public override void Heuristic(float[] actionsOut) {
+        actionsOut[0] = Input.GetAxis("Horizontal");
+        actionsOut[1] = Input.GetAxis("Vertical");
+    }
+
+    void Update() {
+        if (m_LookDir.magnitude > float.Epsilon) {
+            bodyObject.transform.rotation = Quaternion.Lerp(bodyObject.transform.rotation,
+                Quaternion.LookRotation(m_LookDir),
+                Time.deltaTime * 10f);
+        }
+    }
+
+    private float DistanceToCentre(Vector3 location) {
+        var vec = location;
+        vec.y = 0;
+        return vec.magnitude;
+    }
+
+}
---
+++ Assets/Scripts/Pray.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+
+public class Pray : MonoBehaviour
+{
+
+    public float range = 14f;
+    public List<HunterAgent> currentCollisions = new List <HunterAgent> ();
+    public float speed = 300f;
+    Rigidbody m_Rb;
+    EnvironmentParameters m_ResetParams;
+
+    void Start() {
+        m_Rb = gameObject.GetComponent<Rigidbody>();
+        m_ResetParams = Academy.Instance.EnvironmentParameters;
+        var krange = m_ResetParams.GetWithDefault("killRange", 2f);   
+        BoxCollider collider = this.GetComponent(typeof(BoxCollider)) as BoxCollider;
+        collider.size =  new Vector3(krange, 1, krange);
+
+        speed  = m_ResetParams.GetWithDefault("praySpeed", 800);
+    }
+
+    //Track hunters in range
+    void OnTriggerEnter(Collider collision) {
+        var agent = collision.gameObject.GetComponents<HunterAgent>();
+        foreach (HunterAgent a in agent) {
+            if (a != null && !currentCollisions.Contains(a))
+            currentCollisions.Add(a);
+        }
+    }
+    void OnTriggerExit (Collider collision) {
+        var agent = collision.gameObject.GetComponent<HunterAgent>();
+        currentCollisions.Remove(agent);
+    }
+
+    public void Respawn() {
+
+        m_Rb.velocity = Vector3.zero;
+        m_Rb.angularVelocity = Vector3.zero;
+
+        currentCollisions = new List <HunterAgent>();
+        gameObject.transform.localPosition = new Vector3((1 - 2 * Random.value) * 1, 0.5f, (1 - 2 * Random.value) * 1);
+
+        //Get curriculum update from the academy
+        var krange = m_ResetParams.GetWithDefault("killRange", 2f);   
+        BoxCollider collider = this.GetComponent(typeof(BoxCollider)) as BoxCollider;
+        collider.size =  new Vector3(krange, 1, krange);
+
+        speed  = m_ResetParams.GetWithDefault("praySpeed", 800);
+    }
+
+    private void FixedUpdate()
+    {
+        if (currentCollisions.Count > 2 ) { //All agents are in range
+            //var environment = gameObject.transform.parent.gameObject;
+            //var agents = environment.GetComponentsInChildren<Agent>();
+            foreach (var a in currentCollisions) {
+                a.AddReward(5f);
+                a.EndEpisode();
+            }     
+            Respawn();
+        }
+        if (speed > 0f)
+        {
+            Move();
+        }
+
+        if (Mathf.Abs(DistanceToCentre(transform.position)) > range) {
+            var environment = gameObject.transform.parent.gameObject;
+            var agents = environment.GetComponentsInChildren<HunterAgent>();
+            foreach (var a in agents) {
+                a.AddReward(-3f);
+                a.EndEpisode();
+            }    
+            Respawn();
+        } 
+    }
+
+    private void Move()
+    {
+        var environment = gameObject.transform.parent.gameObject;
+        var agents = environment.GetComponentsInChildren<HunterAgent>();
+        Vector3 moveDirection = new Vector3(0f,0f,0f);
+        for (int i = 0; i < 2; i++)  { 
+            moveDirection += transform.position - agents[i].bodyObject.transform.position;
+        } //Face away from hunters and apply force
+        transform.rotation = Quaternion.LookRotation(moveDirection, Vector3.up);
+        Vector3 moveVector = speed * moveDirection.normalized;
+        if (m_Rb.velocity.magnitude < 0.1) {
+            m_Rb.AddForce(moveVector); //kick start movement
+        }
+        m_Rb.AddForce(moveVector);
+    }
+
+    private float DistanceToCentre(Vector3 location) {
+        var vec = location;
+        vec.y = 0;
+        return vec.magnitude;
+    }
+
+}
